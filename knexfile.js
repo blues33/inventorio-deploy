ba9ecac92a73367f84884315d89ed1693a4095bf
Yirey Suh <yrysuh@gmail.com>
2019-01-22 12:40:20 +0900

    add-on info

--- knexfile.js
+++ knexfile.js
@@ -4,12 +4,13 @@ require('dotenv').config();
 module.exports = {
   development: {
     client: 'pg',
-    connection: {
-      host: process.env.PG_HOST,
-      user: process.env.PG_USER,
-      password: process.env.PG_PASSWORD,
-      database: process.env.PG_DATABASE,
-    },
+    connection: process.env.DATABASE_URL,
+    // connection: {
+    //   host: process.env.PG_HOST,
+    //   user: process.env.PG_USER,
+    //   password: process.env.PG_PASSWORD,
+    //   database: process.env.PG_DATABASE,
+    // },
     migrations: {
       directory: path.join(__dirname, '/database/migrations'),
     },
@@ -21,12 +22,13 @@ module.exports = {
   // production will need some adjustment for deployment (process.env.DATABASE_URL)
   production: {
     client: 'pg',
-    connection: {
-      host: process.env.PG_HOST,
-      user: process.env.PG_USER,
-      password: process.env.PG_PASSWORD,
-      database: process.env.PG_DATABASE,
-    },
+    connection: process.env.DATABASE_URL,
+    // connection: {
+    //   host: process.env.PG_HOST,
+    //   user: process.env.PG_USER,
+    //   password: process.env.PG_PASSWORD,
+    //   database: process.env.PG_DATABASE,
+    // },
     migrations: {
       directory: path.join(__dirname, '/database/migrations'),
     },

2604559780868908a719c2870ba5bbd763f723fe
Yirey Suh <yrysuh@gmail.com>
2019-01-22 13:42:05 +0900

    create local db for development

--- knexfile.js
+++ knexfile.js
@@ -4,13 +4,16 @@ require('dotenv').config();
 module.exports = {
   development: {
     client: 'pg',
-    connection: `${process.env.DATABASE_URL}?ssl=true`,
+    // connection: `${process.env.DATABASE_URL}?ssl=true`,
     // connection: {
     //   host: process.env.PG_HOST,
     //   user: process.env.PG_USER,
     //   password: process.env.PG_PASSWORD,
     //   database: process.env.PG_DATABASE,
     // },
+    connection: {
+      database: 'inventorio',
+    },
     migrations: {
       directory: path.join(__dirname, '/database/migrations'),
     },
